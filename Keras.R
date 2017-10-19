19bd0877773ea44b575736e39670a989ad26d21b
SayandB <sayand97@gmail.com>
2017-10-19 13:04:36 +0530

    First commit

---
+++ Keras.R
@@ -0,0 +1,31 @@
+library(keras)
+
+data <- dataset_mnist()
+train <- data$train
+test <- data$test
+Xtrain <- train$x
+Ytrain <- train$y
+Xtest <- test$x
+Ytest <- test$y
+rm(data)
+rm(train)
+rm(test)
+
+Xtrain <- array(Xtrain, dim = c(dim(Xtrain)[1], prod(dim(Xtrain)[-1])))/255
+Xtest <- array(Xtest, dim = c(dim(Xtest)[1], prod(dim(Xtest)[-1])))/255
+
+Ytrain <- to_categorical(Ytrain, 10)
+Ytest <- to_categorical(Ytest, 10)
+
+model <- keras_model_sequential()
+model %>% 
+  layer_dense(units = 784, input_shape = 784, use_bias = TRUE) %>%
+  layer_dropout(rate = 0.25) %>%
+  layer_activation(activation = 'relu') %>%
+  layer_dense(units = 10, use_bias = TRUE) %>%
+  layer_activation(activation = 'softmax')
+model %>% compile(loss='categorical_crossentropy', optimizer = 'adam', metrics = c('accuracy'))
+model %>% fit(Xtrain, Ytrain, epochs = 500, batch_size = 100)
+result <- model %>% evaluate(Xtest, Ytest, batch_size = 100)
+
+
