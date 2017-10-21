784fc9be0646355d7ce8d50a516922c2f2e27976
Sayand Bhattacharya <sayand97@gmail.com>
2017-10-21 13:42:15 +0530

    Update Keras.R

--- Keras.R
+++ Keras.R
@@ -1,5 +1,6 @@
 library(keras)
 
+#Loading MNIST data, comes alongwith the package
 data <- dataset_mnist()
 train <- data$train
 test <- data$test
@@ -11,12 +12,15 @@ rm(data)
 rm(train)
 rm(test)
 
+#Reshaping data and normalisation
 Xtrain <- array(Xtrain, dim = c(dim(Xtrain)[1], prod(dim(Xtrain)[-1])))/255
 Xtest <- array(Xtest, dim = c(dim(Xtest)[1], prod(dim(Xtest)[-1])))/255
 
+#One hot encoding
 Ytrain <- to_categorical(Ytrain, 10)
 Ytest <- to_categorical(Ytest, 10)
 
+#Building model
 model <- keras_model_sequential()
 model %>% 
   layer_dense(units = 784, input_shape = 784, use_bias = TRUE) %>%
@@ -26,6 +30,8 @@ model %>%
   layer_activation(activation = 'softmax')
 model %>% compile(loss='categorical_crossentropy', optimizer = 'adam', metrics = c('accuracy'))
 model %>% fit(Xtrain, Ytrain, epochs = 500, batch_size = 100)
-result <- model %>% evaluate(Xtest, Ytest, batch_size = 100)
 
+#Model performance and outcome
+result <- model %>% evaluate(Xtest, Ytest, batch_size = 100)
+prediction <- model %>% predict_classes(Xtest)
 
